4bf4d91f23b6911779aa99ab96d3731d968ed962
muskandexbytes <muskan.dexbytes@gmail.com>
2024-09-05 18:06:43 +0530

    created register ,login and logout functionality by using livewire

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Livewire\Auth\Register;
+use App\Livewire\Auth\Login;
+use App\Livewire\Auth\Dashboard;
+use App\Livewire\Auth\Logout;
+
+Route::get('/', function () {
+    return redirect()->to('/login');
+});
+
+Route::group(['middleware'=>'guest'], function(){
+    Route::get('/register', Register::class)->name('register');
+    Route::get('/login', Login::class)->name('login');
+});
+
+Route::group(['middleware'=>'auth'], function(){
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
+    Route::get('/logout', Logout::class)->name('logout');
+});
